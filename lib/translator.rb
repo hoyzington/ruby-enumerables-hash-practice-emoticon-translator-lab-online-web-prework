67d3915ef22b6232f1459457a8a5c0ce9cd44825
Scott Hoisington <hoyzington@gmail.com>
2019-11-17 09:17:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,12 +3,12 @@ require "yaml"
 def load_library(string)
   lib = YAML.load_file(string)
   hash = {get_meaning: {}, get_emoticon: {}}
-  lib.each {|key, value|
-    value.each {|eng, jap|
+  lib.each do |key, value|
+    value.each do |eng, jap|
         hash[:get_meaning] = {jap: key}
         hash[:get_emoticon] = {eng: jap}
-    }
-  }
+    end
+  end
   hash
 end
 
